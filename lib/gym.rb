0b85e3e992ab52e72ec402eae28a70b260654814
Daniel Bovington <daniel.bovington@flatironschool.com>
2019-05-19 11:17:42 +0100

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,27 @@
 class Gym
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select { |membership|membership.gym == self }
+  end
+
+  def lifters
+    memberships.map { |membership|membership.lifter  }
+  end
+
+  def combined_lift
+    total = lifters.map do |lifter|
+      lifter.lift_total
+    end
+    total.sum
   end
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,40 @@
 class Lifter
-  attr_reader :name, :lift_total
-
+  attr_accessor :name, :lift_total
+  @@all = []
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select { |membership|membership.lifter == self  }
+  end
+
+  def gyms
+    memberships.map { |membership|membership.gym}
+  end
+
+  def self.average_lift
+    total = Lifter.all.map do |lifter|
+      lifter.lift_total
+    end
+    total.sum / Lifter.all.length
+  end
+
+  def total_cost
+    total = memberships.map do |gym|
+      gym.cost
+    end
+    total.sum
+  end
+
+  def new_membership(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
 end

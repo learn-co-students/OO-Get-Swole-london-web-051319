255ce0d88f37b2af11f47290044d928f93b8ff19
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-05-19 22:08:03 +0100

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
   attr_reader :name
+  # extend Methods::ClassMethods
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.gym == self}
+  end
+
+  def lifters
+    memberships.map { |member| member.lifter}
+  end
+
+  def names
+    lifters.map { |lifter| lifter.name}
+  end
+
+  def liftTotal
+    lifters.reduce(0) {|total, lifter| total += lifter.lift_total}
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,13 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
-
+Rocket = Gym.new("Rocket")
+Rackete = Gym.new("Rackete")
+Felix = Lifter.new("Felix", 50)
+Mario = Lifter.new("Mario", 40)
+Felix.newMembership(30, Rocket)
+Felix.newMembership(70, Rackete)
+Mario.newMembership(80, Rocket)
 binding.pry
 
 puts "Gains!"

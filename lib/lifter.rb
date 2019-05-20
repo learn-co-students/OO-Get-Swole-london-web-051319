740c99637c27410f86e28f6006ec848930aba16a
Alex Johnston <alexjohnston@ymail.com>
2019-05-20 10:10:34 +0100

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,36 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@lifters = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@lifters << self
   end
+
+  def self.all
+    @@lifters
+  end
+
+  def memberships
+    Membership.all.select { |memberships| memberships.lifter == self }
+  end
+
+  def gyms
+    memberships.map {|memberships| memberships.gym}
+  end
+
+  def self.avg_lift_total
+    @@lifters.map{ |lifters| lifters.lift_total }.sum / @@lifters.size
+  end
+
+  def membership_cost
+    memberships.reduce(0) {|total, membership| total + membership.cost}
+  end
+
+  def new_membership(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,6 +2,16 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
+
+dave = Lifter.new("Dave", 100)
+catherine = Lifter.new("Catherine", 80)
+alex = Lifter.new("Alex", 135)
+moorgate = Gym.new("Moorgate")
+wimbledon = Gym.new("Wimbledon")
+dave.new_membership(25, moorgate)
+dave.new_membership(35, wimbledon)
+alex.new_membership(30, moorgate)
+catherine.new_membership(35, moorgate)
 # test code goes here
 
 binding.pry
